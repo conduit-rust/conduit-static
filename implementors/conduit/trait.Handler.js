956567a8b699a3d55ec43958ad2d34fd2148e1d5
Travis CI User <travis@example.org>
2017-08-29 13:09:48 +0000

    Documentation for conduit-rust/conduit-static@f27f37e

---
+++ implementors/conduit/trait.Handler.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["conduit_static"] = ["impl Handler for <a class=\"struct\" href=\"conduit_static/struct.Static.html\" title=\"struct conduit_static::Static\">Static</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["conduit_static"] = {"doc":"","items":[[3,"Static","conduit_static","",null,null],[11,"new","","",0,{"inputs":[{"name":"p"}],"output":{"name":"static"}}],[11,"call","","",0,{"inputs":[{"name":"self"},{"name":"request"}],"output":{"name":"result"}}]],"paths":[[3,"Static"]]};
+initSearch(searchIndex);
